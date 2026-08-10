222f19ae23ac26add225fcd9e15fc8b0726517b0
claudiasrn <perezserranoclaudia@gmail.com>
2026-08-10 20:13:09 +0200

    feat: wire up App.jsx state, navigation, and section rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,54 @@
-export default function App() {
+import { useState } from "react";
+import { HeaderForm } from "./forms/HeaderForm.jsx";
+import { EducationForm } from "./forms/EducationForm.jsx";
+import { ExperienceForm } from "./forms/ExperienceForm.jsx";
+import { LanguagesForm } from "./forms/LanguagesForm.jsx";
+import { ProjectsForm } from "./forms/ProjectsForm.jsx";
+import { SkillsForm } from "./forms/SkillsForm.jsx";
+import { CVPreview } from "./preview/CVPreview.jsx";
+import { SectionMenu } from "./SectionMenu.jsx";
+import { cvData as initialData } from "../data/initialData.js";
 
+export default function App() {
+	const [cvData, setCVData] = useState(initialData);
+	const [currentSection, setCurrentSection] = useState(0);
+	const [submittedSections, setSubmittedSections] = useState({});
+
+	function handleNext() {
+		setSubmittedSections((prev) => ({ ...prev, [currentSection]: true }));
+		setCurrentSection(currentSection + 1);
+	}
+
+	function goToSection(index) {
+		setCurrentSection(index);
+	}
+
+	function renderSection() {
+		switch (currentSection) {
+			case 0:
+				return <HeaderForm handleNext={handleNext} />;
+			case 1:
+				return <ExperienceForm handleNext={handleNext} />;
+			case 2:
+				return <EducationForm handleNext={handleNext} />;
+			case 3:
+				return <ProjectsForm handleNext={handleNext} />;
+			case 4:
+				return <SkillsForm handleNext={handleNext} />;
+			case 5:
+				return <LanguagesForm handleNext={handleNext} />;
+		}
+	}
+
+	return (
+		<>
+			<SectionMenu
+				currentSection={currentSection}
+				submittedSections={submittedSections}
+                goToSection={goToSection}
+			/>
+			{renderSection()}
+			<CVPreview cvData={cvData} />
+		</>
+	);
 }
