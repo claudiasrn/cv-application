cab77c1e0b6a5286deef250e87d19765dd10f3e3
claudiasrn <perezserranoclaudia@gmail.com>
2026-08-12 17:02:37 +0200

    refactor: use EntryCard in ExperienceForm

--- src/components/forms/ExperienceForm.jsx
+++ src/components/forms/ExperienceForm.jsx
@@ -1,3 +1,5 @@
+import { EntryCard } from "../EntryCard";
+
 export function ExperienceForm({ handleNext, updateExperience, experience }) {
 	function handleDeleteExperience(id) {
 		updateExperience(experience.filter((entry) => entry.id !== id));
@@ -25,18 +27,12 @@ export function ExperienceForm({ handleNext, updateExperience, experience }) {
 	}
 
 	function handleAddBullet(id) {
-		const newBullet = {
-			id: Date.now(),
-			text: "",
-		};
-
+		const newBullet = { id: Date.now(), text: "" };
 		updateExperience(
 			experience.map((entry) => {
 				if (entry.id === id) {
-					const bullets = entry.bullets;
-					return { ...entry, bullets: [...bullets, newBullet] };
+					return { ...entry, bullets: [...entry.bullets, newBullet] };
 				}
-
 				return entry;
 			}),
 		);
@@ -70,81 +66,80 @@ export function ExperienceForm({ handleNext, updateExperience, experience }) {
 
 	return (
 		<div className="experience-form">
-			{experience.map((entry) => {
-				return (
-					<div key={entry.id}>
-						<button onClick={() => handleDeleteExperience(entry.id)}>
-							Delete
+			{experience.map((entry) => (
+				<EntryCard
+					key={entry.id}
+					entryId={entry.id}
+					removeEntry={handleDeleteExperience}
+				>
+					<label htmlFor="job-title">Job title</label>
+					<input
+						type="text"
+						id="job-title"
+						value={entry.title}
+						onChange={(e) =>
+							handleFieldChange("title", entry.id, e.target.value)
+						}
+					/>
+					<label htmlFor="company">Company</label>
+					<input
+						type="text"
+						id="company"
+						value={entry.company}
+						onChange={(e) =>
+							handleFieldChange("company", entry.id, e.target.value)
+						}
+					/>
+					<label htmlFor="location">Location</label>
+					<input
+						type="text"
+						id="location"
+						value={entry.location}
+						onChange={(e) =>
+							handleFieldChange("location", entry.id, e.target.value)
+						}
+					/>
+					<label htmlFor="start-date">Start date</label>
+					<input
+						type="text"
+						id="start-date"
+						value={entry.startDate}
+						onChange={(e) =>
+							handleFieldChange("startDate", entry.id, e.target.value)
+						}
+					/>
+					<label htmlFor="end-date">End date</label>
+					<input
+						type="text"
+						id="end-date"
+						value={entry.endDate}
+						onChange={(e) =>
+							handleFieldChange("endDate", entry.id, e.target.value)
+						}
+					/>
+					<div className="responsibilities">
+						{entry.bullets.map((bullet) => (
+							<div key={bullet.id}>
+								<input
+									type="text"
+									value={bullet.text}
+									onChange={(e) =>
+										handleBulletChange(entry.id, bullet.id, e.target.value)
+									}
+								/>
+								<button
+									onClick={() => handleRemoveBullet(entry.id, bullet.id)}
+								>
+									×
+								</button>
+							</div>
+						))}
+						<button onClick={() => handleAddBullet(entry.id)}>
+							Add bullet point
 						</button>
-						<label htmlFor="job-title">Job title</label>
-						<input
-							type="text"
-							id="job-title"
-							value={entry.title}
-							onChange={(e) =>
-								handleFieldChange("title", entry.id, e.target.value)
-							}
-						/>
-						<label htmlFor="company">Company</label>
-						<input
-							type="text"
-							id="company"
-							value={entry.company}
-							onChange={(e) =>
-								handleFieldChange("company", entry.id, e.target.value)
-							}
-						/>
-						<label htmlFor="location">Location</label>
-						<input
-							type="text"
-							id="location"
-							value={entry.location}
-							onChange={(e) =>
-								handleFieldChange("location", entry.id, e.target.value)
-							}
-						/>
-						<label htmlFor="start-date">Start date</label>
-						<input
-							type="text"
-							id="start-date"
-							value={entry.startDate}
-							onChange={(e) =>
-								handleFieldChange("startDate", entry.id, e.target.value)
-							}
-						/>
-						<label htmlFor="end-date">End date</label>
-						<input
-							type="text"
-							id="end-date"
-							value={entry.endDate}
-							onChange={(e) =>
-								handleFieldChange("endDate", entry.id, e.target.value)
-							}
-						/>
-						<div className="responsibilities">
-							{entry.bullets.map((bullet) => (
-								<div key={bullet.id}>
-									<input
-										type="text"
-										value={bullet.text}
-										onChange={(e) =>
-											handleBulletChange(entry.id, bullet.id, e.target.value)
-										}
-									/>
-									<button
-										onClick={() => handleRemoveBullet(entry.id, bullet.id)}
-									>
-										×
-									</button>
-								</div>
-							))}
-							<button onClick={() => handleAddBullet(entry.id)}>
-								Add bullet point
-							</button>
-						</div>
 					</div>
-				);
-			})}
+				</EntryCard>
+			))}
 			<button onClick={addEntry}>+ Add job</button>
 			<button onClick={handleNext}>Next</button>
 		</div>
