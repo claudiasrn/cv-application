6f23c69b5b93294eb1b98a19db13da12e6bd8e07
claudiasrn <perezserranoclaudia@gmail.com>
2026-08-16 14:18:36 +0200

    style: use Trash2 icon for delete button in EntryCard

--- src/components/EntryCard.jsx
+++ src/components/EntryCard.jsx
@@ -1,7 +1,9 @@
+import { Trash2 } from "lucide-react";
+
 export function EntryCard({children, removeEntry, entryId}) {
     return (
         <div className="entry-card">
-            <button onClick={() => removeEntry(entryId)}>Delete</button>
+            <button onClick={() => removeEntry(entryId)}><Trash2/></button>
             {children}
         </div>
     );
